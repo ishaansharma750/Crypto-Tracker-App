1e0276957bf3765cc142cadce4ffd866cc0625f0
Suansh <ishaansharma750@gmail.com>
2023-11-29 21:03:39 +0530

    mouse animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,9 @@ import ComparePage from "./pages/ComparePage";
 import WatchlistPage from "./pages/WatchlistPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import React,{ useEffect } from "react";
+import React from "react";
 
 function App() {
-  var cursor;
-  var cursorPointer;
-
-  // For Cursor Animation 
-  useEffect(() => {
-    cursor = document.getElementById("cursor");
-    cursorPointer = document.getElementById("cursor-pointer");
-
-    document.body.addEventListener("mousemove", function (e) {
-      return (
-        (cursor.style.left = e.clientX + "px"),
-        (cursor.style.top = e.clientY + "px"),
-        (cursorPointer.style.left = e.clientX + "px"),
-        (cursorPointer.style.top = e.clientY + "px")
-      );
-    });
-
-    document.body.addEventListener("mousedown", function (e) {
-      return (
-        (cursor.style.height = "0.5rem"),
-        (cursor.style.width = "0.5rem"),
-        (cursorPointer.style.height = "3rem"),
-        (cursorPointer.style.width = "3rem")
-      );
-    });
-
-    document.body.addEventListener("mouseup", function (e) {
-      return (
-        (cursor.style.height = "0.3rem"),
-        (cursor.style.width = "0.3rem"),
-        (cursorPointer.style.height = "2rem"),
-        (cursorPointer.style.width = "2rem")
-      );
-    });
-  }, []);
-
   return (
     <div className="App">
       <div className="cursor" id="cursor" />
@@ -52,8 +16,8 @@ function App() {
       <ToastContainer />
       <BrowserRouter>
         <Routes>
-           {/* All Page Routes */}
-           
+          {/* All Page Routes */}
+
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/coin/:id" element={<CoinPage />} />
@@ -67,11 +31,3 @@ function App() {
 }
 
 export default App;
-
-// 1. Is it working and functional? Is it responsive? Is it quick? Does it have a good User Experience?
-// 2. What are the features? And how do you explain it to the interviewer.
-// 3. Questions the interviewer can ask.
-
-// How to explain it in interviews -
-// 1. Explain the tech stack - React JS, Coin gecko, framer-motion, MUI components, custom styled ,axios
-// the overall concept.
